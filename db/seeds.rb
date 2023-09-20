46c12ac5de02c9bea6d620e27fb41214a493a37d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-20 06:04:35 +0000

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Todo.create(title: "タスク1")
+Todo.create(title: "タスク2")
+Todo.create(title: "タスク3")
